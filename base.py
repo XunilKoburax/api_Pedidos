039963a0232c0b7df90d7bed2b6fcffe235b77bb
sunekux@gmail.com <sunekux@gmail.com>
2023-08-01 16:05:00 -0600

    Data Models definitions

--- base.py
+++ base.py
@@ -1,2 +1,34 @@
 from fastapi  import FastAPI
 from pydantic import BaseModel
+from datetime import datetime
+
+app  =  FastAPI()
+
+@app.home("/")
+def home():
+    return "Welcome to orders API"
+
+class person(BaseModel):
+    id = int
+    name = str
+    phone = int
+    email = str
+    
+class product(BaseModel):
+    id = int
+    description = str
+    cost = float
+    remaining = float
+    total_cost = float
+    level_school = str
+
+class order():
+    id = int
+    id_product = int
+    id_person = int
+
+class payment(BaseModel):
+    id = int
+    id_order= int
+    payment = float
+    pay_date = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
