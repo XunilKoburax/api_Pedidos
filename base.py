2bb7481d4d39abb22af51516cb8893cb76f4dd3c
XunilKoburax <{ID}+{XunilKoburax}@users.noreply.github.com>
2023-08-01 18:17:17 -0600

    Add validations and corrections to the models.

--- base.py
+++ base.py
@@ -1,35 +1,108 @@
 from fastapi  import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, EmailStr
 from datetime import datetime
+from typing import Optional
+from fastapi import Body, Query, Path
+from enum import Enum
+
+
 
 app  =  FastAPI()
 
-@app.home("/")
-def home():
-    return "Welcome to orders API"
-
 class person(BaseModel):
-    id = int
-    name = str
-    phone = int
-    email = str
+    id: int = Field(
+        gt=0
+    )
+    name : str = Field(
+        ...,
+        min_length =3, 
+        max_length = 50,
+        title="Person Name",
+        examples="Maria"
+    )
+    phone : str = Field(
+        ..., 
+        min_length = 10,
+        max_length = 10,
+        title="Phone number",
+        examples="2345678910"
+        )
+    email : Optional[EmailStr] = Field (
+        title="email",
+        examples="example@example.com"
+    )
+class categorias(Enum):
+    Primaria = "Primaria"
+    Secundaria_t = "Secundaria Tecnica"
+    cbta = "CBTa"
     
 class product(BaseModel):
-    id = int
-    description = str
-    cost = float
-    remaining = float
-    total_cost = float
-    level_school = str
+    id : Optional[int] = Field(
+        ...,
+        title="id",
+        gt=0,
+    )
+    description : str = Field(
+        ...,
+        title="Description",
+        min_length=3,
+        max_length=50,
+        examples="Pans Secundaria Técnica"
+    )
+    cost : Optional[float] =Field(
+        ...,
+        title="Cost",
+        gt=0,
+        examples="200.00"
+    )
+    remaining : Optional[float] =Field(
+        ...,
+        title="Remaining",
+        ge=0,
+        examples="0.00"
+    )
+    total_cost : Optional[float] =Field(
+        ...,
+        title="Total",
+        ge=0,
+        examples="200.00"
+    )
+    categoria : Optional[categorias] = Field(default = None)
 
-class order():
-    id = int
-    id_product = int
-    id_person = int
+
+class order(BaseModel):
+    id : int
+    id_product : int
+    id_person : int
 
 class payment(BaseModel):
-    id = int
-    id_order= int
-    payment = float
-    pay_date = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+    id : Optional[int] = Field(
+        title="id",
+        description="payment id",
+        gt=0,
+        examples="0"
+    )
+    id_order: Optional[int] = Field(
+        title="id_order",
+        description="Order id",
+        gt=0,
+        examples="0"
+    )
+    payment : Optional[float] = Field(
+        title="payment",
+        description="payment $$",
+        gt=0,
+        examples="20"
+    )
+    pay_date : Optional[str] = Field(
+        ...,
+        title="datetime",
+        description="Transaction Time Stamp",
+        examples="11-08-2022 14:00:23",
+        min_length=19,
+        
+        ) 
 
+@app.get("/")
+def home():
+    return "Welcome to orders API"
