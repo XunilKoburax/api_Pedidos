8bd0cc1f0abaac5287a572e4c3005c89e8734984
XunilKoburax <{ID}+{XunilKoburax}@users.noreply.github.com>
2023-08-03 18:26:07 -0600

    Implement JWT in access to add products, create the login form to access, and new models

--- base.py
+++ base.py
@@ -1,14 +1,41 @@
-from fastapi  import FastAPI
+from fastapi  import FastAPI, HTTPException, Depends
 from pydantic import BaseModel, Field, EmailStr
-from datetime import datetime
+from datetime import datetime, timedelta
 from typing import Optional
 from fastapi import Body, Query, Path
 from enum import Enum
-
+from fastapi.security import  OAuth2PasswordBearer, OAuth2PasswordRequestForm,HTTPBearer, HTTPAuthorizationCredentials
+from jwt import PyJWTError
+import jwt
 
 
 app  =  FastAPI()
 
+#Configuracion de jwt
+SECRET_KEY = "secret"  
+ALGORITHM = "HS256"  
+security = HTTPBearer()
+oauth2_scheme = OAuth2PasswordBearer("/login_tocken")
+
+ 
+
+#Definition auth function
+def create_jwt_token(data: dict, secret_key: str = SECRET_KEY, algorithm: str = ALGORITHM, expiration: int = 3600):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(seconds=expiration)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, secret_key, algorithm=algorithm)
+    return encoded_jwt
+
+#Create Auth function
+def get_current_user(credentials: HTTPAuthorizationCredentials = Depends(security)):
+    try:
+        token = credentials.credentials
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload
+    except PyJWTError:
+        raise HTTPException(status_code=401, detail="Token inválido")
+    
 class person(BaseModel):
     id: Optional[int] = Query(
         ...,
@@ -92,9 +119,36 @@ class payment(BaseModel):
         description="Transaction Time Stamp",
         ) 
 
+#test dictionary get an authentication
+fake_db = {
+    "Test":{
+        "username":"Test",
+        "password":"test"
+    }
+}
+class users(BaseModel): 
+    username: Optional[str]=Query(default=None, min_length=3, max_length=15)
+    password: Optional[str]=Query(default=None, min_length=2, max_length=30)
+ 
+
 @app.get("/")
 def home():
     return "Welcome to orders API"
+
+@app.post("/login_tocken")
+def login(form_data : OAuth2PasswordRequestForm = Depends()):
+    if not form_data.username:
+        raise HTTPException (status_code=401, 
+                             detail="Not valid authenticate" ,
+                             headers={"WWW-Authenticate":"Bearier"})
+    if form_data.username == fake_db["Test"]["username"] and\
+        fake_db["Test"]["password"] == form_data.password:
+        print("Entre")
+        token = create_jwt_token({"sub": form_data.username})
+        return {"access_token": token, "token_type": "bearer"}
+    else:
+        print("No entre")
+        raise HTTPException(status_code=401, detail="Credenciales inválidas")
 
 @app.post("/create_person")
 def create_person(Person : person = Body(...)):
@@ -102,9 +156,11 @@ def create_person(Person : person = Body(...)):
     return f"Person {Person.name} has been created"
 
 @app.post("/add_product")
-def create_product(Product : product = Body(...)):
+def create_product(Product : product = Body(...),
+                    current_user: dict = Depends(oauth2_scheme)):
     #Commit the product in DB
-    return f"Product {Product.description} has been created"
+    return f"Product {Product.description} has been created by user {current_user['sub']}"
+
 
 @app.post("/add_order")
 def create_order(Order : Order= Body(...)):
@@ -115,8 +171,6 @@ def create_order(Order : Order= Body(...)):
     #Add product list
     
     return f"Your order id is: {Order.id}"
-
-
 
 @app.post("/addOrder/")
 def add_order(
@@ -133,5 +187,7 @@ def add_order(
         
     }
     return order
+
+
     
 
